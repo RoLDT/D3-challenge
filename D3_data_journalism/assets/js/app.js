bc1d16eaea2708d1e6fd1b6298cafc6b313db4f7
RoLDT <RoLDT@users.noreply.github.com>
2020-11-19 22:40:19 -0600

    Successfully made the svg container (checked the Elements on the Chrome browser

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,26 @@
 // @TODO: YOUR CODE HERE!
+//Create svg container.
+var svgWidth = 900;
+ var svgHeight = 540;
+
+ var margin = {
+     top:20,
+     right: 40,
+     bottom: 80,
+     left: 100
+ };
+
+ var width = svgWidth - margin.left - margin.right;
+ var height = svgHeight - margin.top - margin.bottom;
+
+ var svg = d3
+    .select("#scatter")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+ .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+
+
